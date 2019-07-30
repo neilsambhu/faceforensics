9ea0f08fe322a4bf0751075ed8b81bd99982275b
Neil Sambhu <neilsambhu@gmail.com>
2019-07-30 01:32:07 -0400

    finished parsing JSONs

--- scripts/json-test.py
+++ scripts/json-test.py
@@ -1,5 +1,45 @@
 import json
-with open('conversion_dict.json') as json_file:
-	data = json.load(json_file)
-	for p in data:
-		print('{}: {}'.format(p, data[p]))
+from tqdm import tqdm
+import datetime
+import os
+import sys
+import glob
+
+pathPNGs = open(r'pathPNGs.txt', 'w')
+
+def VerifyDir(dir):
+	if not os.path.exists(dir):
+		sys.exit('Directory \'{}\' does not exist'.format(dir))
+
+def JSON_ParserVideoSequence(pathJSON, dirVideoName, JSON_VideoSequenceNumber):
+	fullPathJSON = os.path.join(os.getcwd(), pathJSON)
+	# print('cwd {}, pathJSON {}'.format(os.getcwd(), pathJSON))
+	# VerifyDir(fullPathJSON)
+	with open(fullPathJSON) as JSON_VideoSequenceFile:
+		data = json.load(JSON_VideoSequenceFile)
+		# print('first {}, last {}'.format(data['first frame'], data['last frame']))
+		firstFrame = data['first frame']
+		lastFrame = data['last frame']
+		framesCount = lastFrame - firstFrame
+		for frameNumber in range(0, framesCount):
+			fileNamePNG = '{}_{}_{}_{}_{}.png'.format(dirVideoName, JSON_VideoSequenceNumber, dirVideoName, JSON_VideoSequenceNumber, frameNumber)
+			pathPNGs.write(fileNamePNG+'\n')
+
+def JSON_ParserVideo(dirBase, dirVideoName):
+	dirJSON = os.path.join(dirBase, dirVideoName, 'faces')
+	# VerifyDir(dirJSON)
+	JSON_Files = glob.glob(os.path.join(dirJSON, '*.json'))
+	for JSON_VideoSequence in JSON_Files:
+		JSON_VideoSequence = os.path.basename(JSON_VideoSequence)
+		pathJSON = os.path.join(dirJSON, JSON_VideoSequence)
+		# print('dirJSON {}, JSON_VideoSequence {}'.format(dirJSON, JSON_VideoSequence))
+		# VerifyDir(pathJSON)
+		JSON_ParserVideoSequence(pathJSON, dirVideoName, os.path.splitext(JSON_VideoSequence)[0])
+
+def JSON_Parser(dirBase='Face2Face_video_information'):
+	# VerifyDir(dirBase)
+	for dirVideoName in os.listdir(dirBase):
+		JSON_ParserVideo(dirBase, dirVideoName)
+
+if __name__ == "__main__":
+	JSON_Parser()
