71b32596ae3d86a4a01da00028fc0f5a344ccffa
Neil Sambhu <neilsambhu@gmail.com>
2019-07-31 04:30:05 +0000

    checking PNGs from JSON are valid

--- scripts/json-test.py
+++ scripts/json-test.py
@@ -13,23 +13,29 @@ def VerifyDir(dir):
 
 def JSON_ParserVideoSequence(pathJSON, dirVideoName, JSON_VideoSequenceNumber):
     fullPathJSON = os.path.join(os.getcwd(), pathJSON)
-    # print('cwd {}, pathJSON {}'.format(os.getcwd(), pathJSON))
     # VerifyDir(fullPathJSON)
     with open(fullPathJSON) as JSON_VideoSequenceFile:
         data = json.load(JSON_VideoSequenceFile)
-        # print('first {}, last {}'.format(data['first frame'], data['last frame']))
         firstFrame = data['first frame']
         lastFrame = data['last frame']
         framesCount = lastFrame - firstFrame
-        for frameNumber in range(0, framesCount+1):
-            fileNamePNG = '{}_{}_{}_{}_{}.png'.format(dirVideoName, JSON_VideoSequenceNumber, dirVideoName, JSON_VideoSequenceNumber, frameNumber)
-            pathPNGs.write(fileNamePNG+'\n')
-            # TODO
-            fullFileNamePNGs = glob.glob(os.path.join('..', 'data', 'FaceForensics_selfreenactment_images', '*', '*', fileNamePNG))
-            if frameNumber is framesCount and os.path.exists(fullFileNamePNGs):
-                print('{} goes beyond framesCount ({}) bounds'.format(fullFileNamePNGs, framesCount))
-            if not os.path.exists(fullFileNamePNGs):
-                print('{} does not exist'.format(fullFileNamePNGs))
+        for frameNumber in range(0, framesCount):
+            fileNamePNG = '{}_{}_{}_{}_{}.png'.format(dirVideoName, 
+                           JSON_VideoSequenceNumber, dirVideoName, 
+                           JSON_VideoSequenceNumber, frameNumber)
+#            pathPNGs.write(fileNamePNG+'\n')
+            fullFileNamePNGs = glob.glob(
+                    os.path.join('..', 'data', 
+                                 'FaceForensics_selfreenactment_images', '*', 
+                                 '*', fileNamePNG))
+            if len(fullFileNamePNGs) is not 2:
+                print('Length of {} files is {}. Contents: {}'.format(fileNamePNG, 
+                      len(fullFileNamePNGs), fullFileNamePNGs))
+            if frameNumber is framesCount and len(fullFileNamePNGs) is 2:
+                print('{} goes beyond framesCount ({}) bounds'.format(fileNamePNG, 
+                      framesCount))
+#            if len(fullFileNamePNGs) is 0:
+#                print('{} does not exist'.format(fileNamePNG))
 
 def JSON_ParserVideo(dirBase, dirVideoName):
     dirJSON = os.path.join(dirBase, dirVideoName, 'faces')
@@ -40,11 +46,12 @@ def JSON_ParserVideo(dirBase, dirVideoName):
         pathJSON = os.path.join(dirJSON, JSON_VideoSequence)
         # print('dirJSON {}, JSON_VideoSequence {}'.format(dirJSON, JSON_VideoSequence))
         # VerifyDir(pathJSON)
-        JSON_ParserVideoSequence(pathJSON, dirVideoName, os.path.splitext(JSON_VideoSequence)[0])
+        JSON_ParserVideoSequence(pathJSON, dirVideoName, 
+                                 os.path.splitext(JSON_VideoSequence)[0])
 
 def JSON_Parser(dirBase='Face2Face_video_information'):
     # VerifyDir(dirBase)
-    for dirVideoName in os.listdir(dirBase):
+    for dirVideoName in tqdm(os.listdir(dirBase)):
         JSON_ParserVideo(dirBase, dirVideoName)
 
 if __name__ == "__main__":
